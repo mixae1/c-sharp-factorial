8ad7d0893ea468341b25f395bb6ff72ae4464585
Anoth <mixaetel@gmail.com>
2022-12-16 10:28:48 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+static BigInteger factorial(int _base)
+{
+    if (_base < 0) return 0;
+    if (_base == 0 || _base == 1) return 1;
+    return factorial(_base - 1) * _base;
+}
+
+System.Console.WriteLine(factorial(100));
