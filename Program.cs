c9a8525d0076a7d68e2aab95cb630173e58bd613
Anoth <mixaetel@gmail.com>
2022-12-16 11:14:34 +0300

    added reading from console

--- Program.cs
+++ Program.cs
@@ -6,4 +6,4 @@ static BigInteger factorial(int _base)
     return factorial(_base - 1) * _base;
 }
 
-System.Console.WriteLine(fact_orial(100));
+Console.WriteLine(factorial(Convert.ToInt32(Console.ReadLine())));
